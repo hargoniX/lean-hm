532c32bd9823bb60d9f0127f4b843f2e18341065
fwuermse <fwuermse@wlgore.com>
2021-09-03 17:52:05 +0200

    Add set proof

--- Hm/sets.lean
+++ Hm/sets.lean
@@ -7,9 +7,11 @@ namespace Set
 
 notation:50 a " ∈ " s:50 => Set.in s a
 
+notation:55 a " ∉ " s:55 => ¬Set.in s a
+
 def pred (p : α → Prop) : Set α := p
 
-notation "{" a "|" p "}" => Set.pred (fun a => p)
+notation "{" a "|" p "}" => Set.pred (λ a => p)
 
 def union (s₁ s₂ : Set α) : Set α :=
   { a | a ∈ s₁ ∨ a ∈ s₂ }
@@ -26,58 +28,37 @@ def subseteq (s₁ s₂ : Set α) : Prop :=
 
 infix:75 " ⊆ " => Set.subseteq
 
-def empty : Set α := fun x => False
+def comp (s₁ s₂ : Set α) : Set α :=
+  { a | a ∈ s₁ ∧ a ∉ s₂ }
+
+infix:80 " \\ " => Set.subseteq
+
+def empty : Set α := λ x => False
 
 notation (priority := high) "∅" => empty
 
 theorem setext {A B : Set α} (h : ∀ x, x ∈ A ↔ x ∈ B) : A = B :=
-  funext (fun x => propext (h x))
+  funext (λ x => propext (h x))
 
 theorem inter_self (A : Set α) : A ∩ A = A :=
-  setext fun x => Iff.intro
-    (fun ⟨h, _⟩ => h)
-    (fun h => ⟨h, h⟩)
+  setext λ x => Iff.intro
+    (λ ⟨h, _⟩ => h)
+    (λ h => ⟨h, h⟩)
 
 theorem inter_empty (A : Set α) : A ∩ ∅ = ∅ :=
-  setext fun x => Iff.intro
-    (fun ⟨_, h⟩ => h)
-    (fun h => False.elim h)
+  setext λ x => Iff.intro
+    (λ ⟨_, h⟩ => h)
+    (λ h => False.elim h)
 
 theorem empty_inter (A : Set α) : ∅ ∩ A = ∅ :=
-  setext fun x => Iff.intro
-    (fun ⟨h, _⟩ => h)
-    (fun h => False.elim h)
+  setext λ x => Iff.intro
+    (λ ⟨h, _⟩ => h)
+    (λ h => False.elim h)
 
 theorem inter.comm (A B : Set α) : A ∩ B = B ∩ A :=
-  setext fun x => Iff.intro
-    (fun ⟨h₁, h₂⟩ => ⟨h₂, h₁⟩)
-    (fun ⟨h₁, h₂⟩ => ⟨h₂, h₁⟩)
-
-theorem inter.right (A B : Set α) (h1: x ∈ A) (h2: x ∈ B): ∀ x, x ∈ A ∧ x ∈ B → x ∈ B := by
-  intro x xiab
-  apply And.right
-  case self =>
-    apply xiab
-
-theorem inter.left (A B : Set α) (h1: x ∈ A) (h2: x ∈ B): ∀ x, x ∈ A ∧ x ∈ B → x ∈ A := by
-  intro a xiab
-  apply And.left
-  case self =>
-    apply xiab
-
-theorem inter.keep (A B : Set α) : ∀ x, x ∈ A → x ∈ B → x ∈ A := by
-  intro a b c
-  exact b
-
-theorem inter.and {A B : Set α} {x : α} : x ∈ A → x ∈ B → x ∈ A ∧ x ∈ B := by
-  intro a b
-  exact And.intro a b
-
-theorem inter.all_and (A B : Set α) : ∀ x, x ∈ A → x ∈ B → x ∈ A ∧ x ∈ B := by
-  intro a
-  exact And.intro
-
--- TODO why can't theorems be used as source for have := ?
+  setext λ x => Iff.intro
+    (λ ⟨h₁, h₂⟩ => ⟨h₂, h₁⟩)
+    (λ ⟨h₁, h₂⟩ => ⟨h₂, h₁⟩)
 
 end Set
 
@@ -98,32 +79,48 @@ theorem eqr (A B : Set α) :  A = B ↔ (∀ x : α, x ∈ A ↔ x ∈ B) := by
   apply Iff.symm
   apply eq
 
-theorem subseteq {A B : Set α} : A ⊆ B ↔ A ∩ B = A := by
+theorem sub_eq_cap {A B : Set α} : A ⊆ B ↔ A ∩ B = A := by
   apply Iff.intro
   case mp =>
     rw [eqr]
     intro seq x
     apply Iff.intro
     case mp =>
-      have h : ∀ x, x ∈ A → x ∈ B := seq
-      have h₁ : x ∈ A → x ∈ B := h x
-      intro int
-      have h₂ : x ∈ A ∧ x ∈ B := int
-      have h₃ : x ∈ A := And.left h₂
-      exact h₃
+      intro xiab
+      apply And.left xiab
     case mpr =>
-      intro xia
-      have h : ∀ x, x ∈ A → x ∈ B := seq
-      have h₁ : x ∈ A → x ∈ B := h x
-      have h₂ : x ∈ B := h₁ xia
-      have h₃ : x ∈ A ∧ x ∈ B := And.intro xia h₂
-      exact h₃
+      intro x₂
+      have h : x ∈ B := (seq x) x₂
+      exact And.intro x₂ h
   case mpr =>
     rw [eqr]
     intro xs x xia
-    have h1 : x ∈ A ∧ x ∈ B ↔ x ∈ A := xs x
-    have h2 : x ∈ A ∧ x ∈ B → x ∈ A := Iff.mp h1
-    have h3 : x ∈ A ∧ x ∈ B := sorry -- TODO how to take the lhs of an implication into account?
-    have h3 : x ∈ B := And.right h3
-    exact h3
-    
+    have h : x ∈ A ∩ B := Iff.mpr (xs x) xia
+    exact And.right h
+
+theorem sub_eq_cup {A B : Set α} : A ⊆ B ↔ A ∪ B = B := by
+  apply Iff.intro
+  case mp =>
+    intro seq
+    rw [eqr]
+    intro x
+    apply Iff.intro
+    case mp =>
+      intro xiaob
+      apply (Or.elim xiaob) (seq x)
+      intro b
+      exact b
+    case mpr =>
+      intro xib
+      exact Or.inr xib
+  case mpr =>
+    rw [eqr]
+    intro xs x xia
+    have h : x ∈ B := Iff.mp (xs x) (Or.inl xia)
+    exact h
+
+theorem not_cap_eq_cap {A B : Set α} : A \ (A ∩ B) = A \ B := sorry
+
+-- example for relation syntax
+theorem ex3 : (10000, 300000) ∈ { (x, y) | x < y } ∩ { (x, y) | x = 10000 } :=
+  sorry
