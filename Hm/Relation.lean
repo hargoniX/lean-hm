00e6b6a3258add127a802d86229427f1e5af4f3b
Henrik Böving <hargonix@gmail.com>
2021-09-06 15:21:17 +0200

    Formalize Theorem 1.2.1

--- Hm/Relation.lean
+++ Hm/Relation.lean
@@ -34,7 +34,7 @@ class EquivalenceRel {α : Type _} (rel : RelationOn α) where
   reflexive : reflexive rel
   symmetrical : symmetrical rel
   transitive : transitive rel
-  
+
 /-
 This instance allows us to use ≈ (denoted with \~~) when we know there
 exists an equivalence relation on α.
@@ -44,8 +44,17 @@ variable (α : Type u) (a b : α) (R : RelationOn α) [EquivalenceRel R]
 instance {rel : RelationOn α} [EquivalenceRel rel] : HasEquiv α where
   Equiv := λ a b => (a, b) ∈ rel
 
-def equivalence_class {α : Type u} (R : RelationOn α) [EquivalenceRel R] (a : α) : Set α := {x | (a, x) ∈ R}
+def equivalence_class {α : Type u} (R : RelationOn α) [EquivalenceRel R] (a : α) : Set α := {x | a ≈ x }
 def id_rel (α : Type u) : RelationOn α := { t | t.fst = t.snd }
+
+theorem equivalence_classes_disjunct {α : Type u} (R: RelationOn α) [eqr: EquivalenceRel R] (a b : α) (a_nequiv_b: ¬(a ≈ b)) : equivalence_class R a ∩ equivalence_class R b = ∅ := by
+  apply (eq_empty_iff_all_notin _).mpr
+  intro x hx
+  have a_equiv_x : a ≈ x := hx.left
+  have b_equiv_x : b ≈ x := hx.right
+  have x_equiv_b : x ≈ b := eqr.symmetrical b x b_equiv_x
+  have a_equiv_b : a ≈ b := eqr.transitive a x b ⟨a_equiv_x, x_equiv_b⟩
+  exact a_nequiv_b a_equiv_b
 
 theorem id_rel_reflexive (α : Type u) : reflexive $ id_rel α := λ _ => rfl
   
